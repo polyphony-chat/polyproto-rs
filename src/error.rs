84c60b9c4916c568dde24b82d55afab098a20a60
bitfl0wer <florian@pro-weber.com>
2024-02-03 12:37:46 +0100

    Add thiserror for custom errors

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use thiserror::Error;
+
+use crate::SignatureType;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Signature type mismatch: {0:?} != {1:?}")]
+    SignatureTypeMismatch(SignatureType, SignatureType),
+}
