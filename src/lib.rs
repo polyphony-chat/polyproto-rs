d71f454fdb02324a8515c57762e2a3c2da1fc495
bitfl0wer <florian@pro-weber.com>
2024-02-03 15:47:38 +0100

    Impl std::fmt::Display for SignatureAlgorithm

--- src/lib.rs
+++ src/lib.rs
@@ -44,3 +44,36 @@ pub enum SignatureAlgorithm {
     CRYSTALS_DILITHIUM3_AES,
     CRYSTALS_DILITHIUM5_AES,
 }
+
+impl std::fmt::Display for SignatureAlgorithm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SignatureAlgorithm::ECDSA_SECP256R1_SHA256 => write!(f, "ECDSA_SECP256R1_SHA256"),
+            SignatureAlgorithm::ECDSA_SECP384R1_SHA384 => write!(f, "ECDSA_SECP384R1_SHA384"),
+            SignatureAlgorithm::ECDSA_SECP521R1_SHA512 => write!(f, "ECDSA_SECP521R1_SHA512"),
+            SignatureAlgorithm::ED25519 => write!(f, "ED25519"),
+            SignatureAlgorithm::ED448 => write!(f, "ED448"),
+            SignatureAlgorithm::RSASSA_PKCS1_v_1_5_SHA256 => write!(f, "RSASSA_PKCS1_v_1_5_SHA256"),
+            SignatureAlgorithm::RSASSA_PKCS1_v_1_5_SHA384 => write!(f, "RSASSA_PKCS1_v_1_5_SHA384"),
+            SignatureAlgorithm::RSASSA_PKCS1_v_1_5_SHA512 => write!(f, "RSASSA_PKCS1_v_1_5_SHA512"),
+            SignatureAlgorithm::RSASSA_PSS_SHA256 => write!(f, "RSASSA_PSS_SHA256"),
+            SignatureAlgorithm::RSASSA_PSS_SHA384 => write!(f, "RSASSA_PSS_SHA384"),
+            SignatureAlgorithm::RSASSA_PSS_SHA512 => write!(f, "RSASSA_PSS_SHA512"),
+            SignatureAlgorithm::ECDSA_BRAINPOOLP256R1_SHA256 => {
+                write!(f, "ECDSA_BRAINPOOLP256R1_SHA256")
+            }
+            SignatureAlgorithm::ECDSA_BRAINPOOLP384R1_SHA384 => {
+                write!(f, "ECDSA_BRAINPOOLP384R1_SHA384")
+            }
+            SignatureAlgorithm::ECDSA_BRAINPOOLP512R1_SHA512 => {
+                write!(f, "ECDSA_BRAINPOOLP512R1_SHA512")
+            }
+            SignatureAlgorithm::CRYSTALS_DILITHIUM2 => write!(f, "CRYSTALS_DILITHIUM2"),
+            SignatureAlgorithm::CRYSTALS_DILITHIUM3 => write!(f, "CRYSTALS_DILITHIUM3"),
+            SignatureAlgorithm::CRYSTALS_DILITHIUM5 => write!(f, "CRYSTALS_DILITHIUM5"),
+            SignatureAlgorithm::CRYSTALS_DILITHIUM2_AES => write!(f, "CRYSTALS_DILITHIUM2_AES"),
+            SignatureAlgorithm::CRYSTALS_DILITHIUM3_AES => write!(f, "CRYSTALS_DILITHIUM3_AES"),
+            SignatureAlgorithm::CRYSTALS_DILITHIUM5_AES => write!(f, "CRYSTALS_DILITHIUM5_AES"),
+        }
+    }
+}
