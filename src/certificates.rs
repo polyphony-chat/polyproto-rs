03cd53c4671d4c955d31f9af1fe46542bfa2c4f5
bitfl0wer <florian@pro-weber.com>
2024-02-03 12:46:50 +0100

    Add documentation, change `sign` to `try_sign` which fails on signature type mismatch between pub/privkey

--- src/certificates.rs
+++ src/certificates.rs
@@ -4,17 +4,23 @@
 
 use signature::Signer;
 
-use crate::SignatureType;
+use crate::keys::PublicKey;
+use crate::{HasSignatureType, SignatureType};
 
+/// A certificate signing request (CSR) for a polyproto identity certificate.
+/// `expiry` is an optional field that specifies the expiry UNIX timestamp of the requested certificate.
+/// The certificate authority can choose to ignore this field and issue a certificate with a different expiry.
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct IdCsr {
-    pub pub_key: String,
+    pub pub_key: PublicKey,
     pub federation_id: FederationId,
     pub session_id: String,
     pub expiry: Option<u64>,
 }
 
 impl IdCsr {
+    /// Makes a new [`IdCertTBS`] from the given [`IdCsr`] by adding information about the
+    /// certificate's serial number and expiry UNIX timestamp.
     pub fn to_id_cert_tbs(&self, expiry: u64, serial: &str) -> IdCertTBS {
         IdCertTBS {
             pub_key: self.pub_key.clone(),
@@ -37,6 +43,8 @@ impl From<IdCertTBS> for IdCsr {
     }
 }
 
+/// A unique identifier for actors in the polyproto federation model, consisting of an actor name,
+/// domain, and top-level domain (TLD).
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct FederationId {
     pub actor_name: String,
@@ -44,9 +52,11 @@ pub struct FederationId {
     pub tld: String,
 }
 
+/// A signed identity certificate for a polyproto actor, signed by a certificate authority.
+/// The certificate authority is represented by the `domain` and `tld` fields of the [`federation_id`](FederationId).
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct IdCert {
-    pub pub_key: String,
+    pub pub_key: PublicKey,
     pub federation_id: FederationId,
     pub session_id: String,
     pub expiry: u64,
@@ -54,15 +64,22 @@ pub struct IdCert {
     pub signature: Signature<SignatureType>,
 }
 
+/// A signature for a certificate, consisting of a signature type and the signature string itself.
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct Signature<T: Copy> {
     pub signature_type: T,
     pub signature: String,
 }
 
+/// An unsigned certificate to-be-signed (TBS) for a polyproto actor, containing an expiry UNIX
+/// timestamp and a unique serial number in addition to the information supplied in a [`IdCsr`].
+///
+/// A signing authority can sign this TBS to produce a [`IdCert`]. Signing authorities must verify
+/// all information in the TBS to ensure that the certificate is valid and up to the polyproto
+/// specification's standards.
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
 pub struct IdCertTBS {
-    pub pub_key: String,
+    pub pub_key: PublicKey,
     pub federation_id: FederationId,
     pub session_id: String,
     pub expiry: u64,
@@ -87,15 +104,24 @@ impl IdCertTBS {
         bytes.to_vec()
     }
 
-    pub fn sign<P: Signer<Signature<SignatureType>>>(self, private_key: P) -> IdCert {
+    pub fn try_sign<P: Signer<Signature<SignatureType>> + HasSignatureType>(
+        self,
+        private_key: P,
+    ) -> Result<IdCert, crate::error::Error> {
+        if private_key.signature_type() != self.pub_key.signature_type() {
+            Err(crate::error::Error::SignatureTypeMismatch(
+                private_key.signature_type(),
+                self.pub_key.signature_type(),
+            ))?
+        }
         let signature = private_key.sign(&self.to_bytes());
-        IdCert {
+        Ok(IdCert {
             pub_key: self.pub_key,
             federation_id: self.federation_id,
             session_id: self.session_id,
             expiry: self.expiry,
             serial: self.serial,
             signature,
-        }
+        })
     }
 }
