4507786c9c91af152b358da4851ab32fa0fa8db5
bitfl0wer <florian@pro-weber.com>
2024-02-16 21:07:35 +0100

    Crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,27 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+/*!
+
+Generic rust types and traits to quickly get a
+[polyproto](https://docs.polyphony.chat/Protocol%20Specifications/core/) implementation up and
+running.
+
+## Implementing polyproto
+
+Start by implementing the traits in [`crate::signature`] and [`crate::key`]. You can then
+use the [`crate::cert`] types to build certificates using your implementations of the
+aformentioned traits.
+
+## Cryptography
+
+This crate provides no cryptographic functionality whatsoever; its sole purpose is to aid in
+implementing polyproto by transforming the
+[polyproto specification](https://docs.polyphony.chat/Protocol%20Specifications/core/) into
+well-defined yet adaptable Rust types.
+
+*/
+
 #[deny(missing_docs)]
 
 /// Generic polyproto certificate types and traits.
