4dddbf395b270512f25c8d3c2d0be4ea2722d44e
bitfl0wer <florian@pro-weber.com>
2024-02-29 19:31:11 +0100

    add little todo note

--- src/signature.rs
+++ src/signature.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+// TODO: Is this trait needed?
+
 use spki::{AlgorithmIdentifierOwned, SignatureBitStringEncoding};
 
 /// A signature value, generated using a [SignatureAlgorithm]
