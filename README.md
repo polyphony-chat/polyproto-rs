18a2789f840b3903d5310af420c21bd276e8627a
bitfl0wer <florian@pro-weber.com>
2024-02-29 16:45:12 +0100

    Bump version, change README

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ This crate extends upon types offered by [der](https://crates.io/crates/der) and
 [spki](https://crates.io/crates/spki). As such, these crates are required dependencies for
 projects looking to implement polyproto.
 
-Start by implementing the traits in [crate::signature] and [crate::key]. You can then
-use the [crate::cert] types to build certificates using your implementations of the
-aformentioned traits.
+Start by implementing the trait [crate::signature::Signature] for a signature algorithm of your
+choice. Popular crates for cryptography and signature algorithms supply their own `PublicKey` and
+`PrivateKey` types. You should extend upon these types with your own structs and implement the
+[crate::key] traits for these new structs.
+
+You can then use the [crate::certs] types to build certificates using your implementations of the
+aforementioned traits.
 
 ## Cryptography
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,25 @@
 
 /*!
 
+# polyproto
+
 (Generic) Rust types and traits to quickly get a
 [polyproto](https://docs.polyphony.chat/Protocol%20Specifications/core/) implementation up and
 running.
 
 ## Implementing polyproto
 
-Start by implementing the traits in [crate::signature] and [crate::key]. You can then
-use the [crate::cert] types to build certificates using your implementations of the
-aformentioned traits.
+This crate extends upon types offered by [der](https://crates.io/crates/der) and
+[spki](https://crates.io/crates/spki). As such, these crates are required dependencies for
+projects looking to implement polyproto.
+
+Start by implementing the trait [crate::signature::Signature] for a signature algorithm of your
+choice. Popular crates for cryptography and signature algorithms supply their own `PublicKey` and
+`PrivateKey` types. You should extend upon these types with your own structs and implement the
+[crate::key] traits for these new structs.
+
+You can then use the [crate::certs] types to build certificates using your implementations of the
+aforementioned traits.
 
 ## Cryptography
 
