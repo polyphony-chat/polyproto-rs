f1d70516c2aca22642cd4fdc2395d124f16ee5bb
bitfl0wer <florian@pro-weber.com>
2024-04-11 12:00:39 +0200

    Create mod errors

---
+++ src/errors/mod.rs
@@ -0,0 +1,8 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+/// "Base" error types which can be combined into "composite" error types
+pub mod base;
+/// "Composite" error types which consist of one or more "base" error types
+pub mod composite;
