74f3afd9768cd55a16173037074379a22bd60628
bitfl0wer <florian@pro-weber.com>
2024-04-14 20:48:43 +0200

    Add to_le_bits, to_be_bits, to_bitstring, impl tryfrom extension for keyusages, impl from keyusages for attribute, impl from keyusages for extension

--- src/certs/capabilities/key_usage.rs
+++ src/certs/capabilities/key_usage.rs
@@ -5,7 +5,7 @@
 use std::str::FromStr;
 
 use der::asn1::{BitString, OctetString, SetOfVec};
-use der::{Any, Encode, Tag, Tagged};
+use der::{Any, Decode, Encode, Tag, Tagged};
 use spki::ObjectIdentifier;
 use x509_cert::attr::Attribute;
 use x509_cert::ext::Extension;
@@ -83,25 +83,133 @@ impl TryFrom<u32> for KeyUsage {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+/// The KeyUsages struct is a collection of KeyUsage variants.
 pub struct KeyUsages {
+    /// Vector of KeyUsage variants.
     pub key_usages: Vec<KeyUsage>,
 }
 
 impl KeyUsages {
+    /// Creates a new KeyUsages struct from a slice of KeyUsage variants.
     pub fn new(key_usages: &[KeyUsage]) -> Self {
         KeyUsages {
             key_usages: key_usages.to_vec(),
         }
     }
+
+    /// Converts a byte slice to a KeyUsages struct, given that the byte slice is a bitstring
+    /// representing the KeyUsages.
+    ///
+    /// RFC 5280 says:
+    /// ```text
+    /// KeyUsage ::= BIT STRING {
+    ///     digitalSignature        (0),
+    ///     nonRepudiation          (1), -- recent editions of X.509 have
+    ///                                  -- renamed this bit to contentCommitment
+    ///     keyEncipherment         (2),
+    ///     dataEncipherment        (3),
+    ///     keyAgreement            (4),
+    ///     keyCertSign             (5),
+    ///     cRLSign                 (6),
+    ///     encipherOnly            (7),
+    ///     decipherOnly            (8) }
+    /// ```
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, InvalidInput> {
+        /* Below, we are doing some operations on bits. RFC 5280 says:
+        KeyUsage ::= BIT STRING {
+            digitalSignature        (0),
+            nonRepudiation          (1), -- recent editions of X.509 have
+                                -- renamed this bit to contentCommitment
+            keyEncipherment         (2),
+            dataEncipherment        (3),
+            keyAgreement            (4),
+            keyCertSign             (5),
+            cRLSign                 (6),
+            encipherOnly            (7),
+            decipherOnly            (8) }
+
+        It is now our task to check, which bits (0 - 8) are set, and to construct KeyUsage variants
+        from this information.
+        Note, that BitStrings represent the bits they store in big endian order, meaning that the
+        most significant bit (MSB) is the first bit in the BitString.
+        */
+        let bitstring = BitString::from_bytes(bytes)?;
+        // The "starting number" is 2 to the power of len(bitstring) [<-pseudocode].
+        // For a bit of length 8, this would be 2^8=256.
+        let mut starting_number = 2u32.pow(bitstring.bit_len() as u32);
+        let mut key_usages = Vec::new();
+        // We iterate over all bits, check if the current bit is set and try to convert the
+        // current value of starting_number to a KeyUsage variant. On every iteration, we divide
+        // starting_number by two, until it equals 1 and thus cannot be divided any further.
+        for bit in bitstring.bits() {
+            match bit {
+                true => 1u8,
+                false => {
+                    divide_starting_number(&mut starting_number);
+                    continue;
+                }
+            };
+            key_usages.push(KeyUsage::try_from(starting_number)?);
+            if starting_number == 1 {
+                // Stop the loop if starting_number is already 1.
+                break;
+            }
+            divide_starting_number(&mut starting_number);
+        }
+        Ok(KeyUsages::new(&key_usages))
+    }
+
+    /// Converts the KeyUsages to a bitstring in little endian order.
+    pub fn to_le_bits(mut self) -> [bool; 9] {
+        self.key_usages.sort();
+        self.key_usages.dedup();
+        let mut bit_vec = [false; 9];
+        for item in self.key_usages.into_iter() {
+            match item {
+                KeyUsage::DigitalSignature => bit_vec[0] = true,
+                KeyUsage::ContentCommitment => bit_vec[1] = true,
+                KeyUsage::KeyEncipherment => bit_vec[2] = true,
+                KeyUsage::DataEncipherment => bit_vec[3] = true,
+                KeyUsage::KeyAgreement => bit_vec[4] = true,
+                KeyUsage::KeyCertSign => bit_vec[5] = true,
+                KeyUsage::CrlSign => bit_vec[6] = true,
+                KeyUsage::EncipherOnly => bit_vec[7] = true,
+                KeyUsage::DecipherOnly => bit_vec[8] = true,
+            }
+        }
+        bit_vec
+    }
+
+    /// Converts the KeyUsages to a bitstring in big endian order.
+    pub fn to_be_bits(self) -> [bool; 9] {
+        let mut bit_vec = self.to_le_bits();
+        bit_vec.reverse();
+        bit_vec
+    }
+
+    /// Converts the KeyUsages to a [BitString].
+    pub fn to_bitstring(self) -> BitString {
+        let bits = self.to_be_bits();
+        let mut bytes = bits
+            .iter()
+            .map(|x| if *x { 1 } else { 0 })
+            .collect::<Vec<u8>>();
+        while bytes[0] == 0 {
+            bytes.remove(0);
+        }
+        let mut unused_bits: u8 = 0;
+        while bytes.len() % 8 != 0 {
+            bytes.push(0);
+            unused_bits += 1;
+        }
+        BitString::new(unused_bits, bytes)
+            .expect("Error when converting KeyUsages to BitString. Please report this error")
+    }
 }
 
 impl From<KeyUsages> for BitString {
     fn from(value: KeyUsages) -> Self {
-        let mut number: usize = 0;
-        for item in value.key_usages.iter() {
-            number += *item as usize;
-        }
-        BitString::from_bytes(number.to_be_bytes().as_slice()).expect("Error when trying to convert KeyUsages to BitString. Please report this error to https://github.com/polyphony-chat/polyproto")
+        value.to_bitstring()
     }
 }
 
@@ -129,48 +237,7 @@ impl TryFrom<Attribute> for KeyUsages {
             }
         };
         let inner_value = value.values.get(0).expect("Illegal state. Please report this error to https://github.com/polyphony-chat/polyproto");
-        /* Below, we are doing some operations on bits. RFC 5280 says:
-        KeyUsage ::= BIT STRING {
-            digitalSignature        (0),
-            nonRepudiation          (1), -- recent editions of X.509 have
-                                -- renamed this bit to contentCommitment
-            keyEncipherment         (2),
-            dataEncipherment        (3),
-            keyAgreement            (4),
-            keyCertSign             (5),
-            cRLSign                 (6),
-            encipherOnly            (7),
-            decipherOnly            (8) }
-
-        It is now our task to check, which bits (0 - 8) are set, and to construct KeyUsage variants
-        from this information.
-        Note, that BitStrings represent the bits they store in big endian order, meaning that the
-        most significant bit (MSB) is the first bit in the BitString.
-        */
-        let bitstring = BitString::from_bytes(inner_value.value())?;
-        // The "starting number" is 2 to the power of len(bitstring) [<-pseudocode].
-        // For a bit of length 8, this would be 2^8=256.
-        let mut starting_number = 2u32.pow(bitstring.bit_len() as u32);
-        let mut key_usages = Vec::new();
-        // We iterate over all bits, check if the current bit is set and try to convert the
-        // current value of starting_number to a KeyUsage variant. On every iteration, we divide
-        // starting_number by two, until it equals 1 and thus cannot be divided any further.
-        for bit in bitstring.bits() {
-            match bit {
-                true => 1u8,
-                false => {
-                    divide_starting_number(&mut starting_number);
-                    continue;
-                }
-            };
-            key_usages.push(KeyUsage::try_from(starting_number)?);
-            if starting_number == 1 {
-                // Stop the loop if starting_number is already 1.
-                break;
-            }
-            divide_starting_number(&mut starting_number);
-        }
-        Ok(KeyUsages::new(&key_usages))
+        KeyUsages::from_bytes(inner_value.value())
     }
 }
 
@@ -184,19 +251,47 @@ impl TryFrom<Extension> for KeyUsages {
     type Error = InvalidInput;
 
     fn try_from(value: Extension) -> Result<Self, Self::Error> {
-        todo!()
+        if value.extn_id.to_string().as_str() != OID_KEY_USAGE {
+            return Err(InvalidInput::IncompatibleVariantForConversion {
+                reason: format!(
+                    "Expected OID {} for KeyUsages, found OID {}",
+                    OID_KEY_USAGE, value.extn_id
+                ),
+            });
+        }
+        // here we need to NOT just do .as_bytes() but to somehow get the actual value in this
+        // octetstring
+        // TODO
+        KeyUsages::from_bytes(value.extn_value.as_bytes())
     }
 }
 
 impl From<KeyUsages> for Attribute {
     fn from(value: KeyUsages) -> Self {
-        todo!()
+        let mut sov = SetOfVec::new();
+        let bitstring = value.to_bitstring();
+        sov.insert(
+            Any::from_der(&bitstring.to_der().expect(
+                "Error occurred when converting blah blah blah this will be tryfrom anyways soon",
+            ))
+            .expect("this is tryfrom soon lalalalalla"),
+        )
+        .expect("wow");
+        dbg!(&sov);
+        Attribute {
+            oid: ObjectIdentifier::from_str(OID_KEY_USAGE).expect(
+                "Error occurred when converting KeyUsages to ObjectIdentifier. Please report this crash",
+            ),
+            values: sov,
+        }
     }
 }
 
 impl From<KeyUsages> for Extension {
     fn from(value: KeyUsages) -> Self {
-        todo!()
+        let bitstring = value.to_bitstring();
+        let any = Any::from_der(&bitstring.to_der().expect("guh")).expect("Error occurred when converting KeyUsages to BitString. Please report this crash at https://github.com/polyphony-chat/polyproto");
+        Extension { extn_id: ObjectIdentifier::from_str(OID_KEY_USAGE).expect("Error occurred when converting KeyUsages to ObjectIdentifier. Please report this crash at https://github.com/polyphony-chat/polyproto"), critical: true, extn_value: OctetString::new(any.to_der().expect("Error occurred when converting KeyUsages to DER. Please report this crash at https://github.com/polyphony-chat/polyproto")).expect("Error occurred when creating OctetString. Please report this crash at https://github.com/polyphony-chat/polyproto") }
     }
 }
 
@@ -213,6 +308,23 @@ mod test {
             KeyUsage::KeyAgreement,
         ]);
         let bitstring = BitString::from(key_usages);
-        dbg!(bitstring)
+        dbg!(bitstring);
+    }
+
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+    #[cfg_attr(not(target_arch = "wasm32"), test)]
+    fn key_usages_vec_sorts_correctly() {
+        #[allow(clippy::useless_vec)]
+        let mut vec = vec![
+            KeyUsage::ContentCommitment,
+            KeyUsage::EncipherOnly,
+            KeyUsage::DataEncipherment,
+            KeyUsage::DigitalSignature,
+        ];
+        vec.sort();
+        assert!(*vec.get(0).unwrap() == KeyUsage::DigitalSignature);
+        assert!(*vec.get(1).unwrap() == KeyUsage::ContentCommitment);
+        assert!(*vec.get(2).unwrap() == KeyUsage::DataEncipherment);
+        assert!(*vec.get(3).unwrap() == KeyUsage::EncipherOnly);
     }
 }
