f7fae073747af7ba7308b0eb58344ffad0d9366f
bitfl0wer <florian@pro-weber.com>
2024-04-18 19:51:43 +0200

    Add schemas

---
+++ src/api/types/schemas/authentication.rs
@@ -0,0 +1,31 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use crate::certs::idcert::IdCert;
+use crate::errors::composite::ConversionError;
+use crate::key::PublicKey;
+use crate::signature::Signature;
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+pub struct NewIdCert {
+    pub actor_name: String,
+    pub csr: String,
+    pub auth_payload: Option<String>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+pub struct NewIdCertCreated {
+    pub id_cert: String,
+    pub token: String,
+}
+
+impl<S: Signature, P: PublicKey<S>> TryFrom<NewIdCertCreated> for IdCert<S, P> {
+    type Error = ConversionError;
+
+    fn try_from(value: NewIdCertCreated) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
