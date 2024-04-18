7a8ecd12706eb95e8941f0ca3fb133e55d77dcf7
bitfl0wer <florian@pro-weber.com>
2024-04-18 21:49:52 +0200

    Add new schemas, document things

--- src/api/types/schemas/authentication.rs
+++ src/api/types/schemas/authentication.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use crate::api::types::entities::Challenge;
+use crate::api::types::entities::{Challenge, CompletedChallenge};
 use crate::certs::idcert::IdCert;
 use crate::errors::composite::ConversionError;
 use crate::key::PublicKey;
@@ -10,38 +10,95 @@ use crate::signature::Signature;
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub struct CreateSessionSchema {
+/// Schema for creating a new session.
+///
+/// `/p2core/session/trust`
+pub struct CreateSessionRequest {
+    /// The name of the actor that is creating the session.
     pub actor_name: String,
+    /// PEM encoded [IdCsr]
     pub csr: String,
+    /// Optional authentication payload.
     pub auth_payload: Option<String>,
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub struct SessionCreatedSchema {
+/// Response counterpart of [CreateSessionSchema].
+pub struct CreateSessionResponse {
+    /// PEM encoded [IdCert]
     pub id_cert: String,
+    /// An authentication (bearer) token.
     pub token: String,
 }
 
-impl<S: Signature, P: PublicKey<S>> TryFrom<SessionCreatedSchema> for IdCert<S, P> {
+impl<S: Signature, P: PublicKey<S>> TryFrom<CreateSessionResponse> for IdCert<S, P> {
     type Error = ConversionError;
 
-    fn try_from(value: SessionCreatedSchema) -> Result<Self, Self::Error> {
+    fn try_from(value: CreateSessionResponse) -> Result<Self, Self::Error> {
         Self::from_pem(&value.id_cert)
     }
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-pub struct GetChallengeResponseSchema {
+/// Schema for getting a challenge. Can be converted to [Challenge] using [TryFrom].
+///
+/// `/p2core/challenge`
+pub struct GetChallengeResponse {
+    /// The challenge string.
     pub challenge: String,
+    /// UNIX timestamp when the challenge expires.
     pub expires: u64,
 }
 
-impl TryFrom<GetChallengeResponseSchema> for Challenge {
+impl TryFrom<GetChallengeResponse> for Challenge {
     type Error = ConversionError;
 
-    fn try_from(value: GetChallengeResponseSchema) -> Result<Self, Self::Error> {
+    fn try_from(value: GetChallengeResponse) -> Result<Self, Self::Error> {
         Challenge::new(&value.challenge, value.expires)
     }
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+/// Completed challenge payload.
+pub struct ChallengePayload {
+    /// The challenge string.
+    pub challenge: String,
+    /// The signature of the challenge.
+    pub signature: String,
+}
+
+impl<S: Signature> From<CompletedChallenge<S>> for ChallengePayload {
+    fn from(value: CompletedChallenge<S>) -> Self {
+        Self {
+            challenge: value.challenge.to_string(),
+            signature: value.signature.to_string(),
+        }
+    }
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+/// Identify payload to log a session in.
+///
+/// `/p2core/session/identify`
+pub struct IdentifyRequest {
+    /// A completed challenge.
+    pub challenge_signature: ChallengePayload,
+    /// PEM encoded [IdCert]
+    pub id_cert: String,
+    /// Optional authentication payload.
+    pub auth_payload: Option<String>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+/// Response counterpart of [IdentifyRequest].
+pub struct IdentifyResponse {
+    /// An authentication (bearer) token.
+    pub token: String,
+    /// Optional payload from the server.
+    pub payload: Option<String>,
 }
