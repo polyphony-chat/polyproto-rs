a07a10c52ca10579d63cb747a05de49065cfbd27
bitfl0wer <florian@pro-weber.com>
2024-05-19 18:52:26 +0200

    remove unused import

--- src/constraints.rs
+++ src/constraints.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use std::vec;
-
 use der::asn1::Ia5String;
 use der::Length;
 use log::debug;
