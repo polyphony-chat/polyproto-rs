61b1b6dc66985c0b2cc2c7c65c882ec6c19c8b52
bitfl0wer <florian@pro-weber.com>
2024-05-25 15:57:11 +0200

    Add missing route definitions

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -15,18 +15,68 @@ pub mod routes {
     }
 
     pub mod core {
-        use super::Route;
+        pub mod v1 {
+            use super::super::Route;
 
-        pub static GET_CHALLENGE_STRING: Route = Route {
-            method: http::Method::GET,
-            path: "/.p2/core/v1/challenge",
-        };
+            pub static GET_CHALLENGE_STRING: Route = Route {
+                method: http::Method::GET,
+                path: "/.p2/core/v1/challenge",
+            };
 
-        pub static ROTATE_SERVER_IDENTITY_KEY: Route = Route {
-            method: http::Method::PUT,
-            path: "/.p2/core/v1/key/server",
-        };
+            pub static ROTATE_SERVER_IDENTITY_KEY: Route = Route {
+                method: http::Method::PUT,
+                path: "/.p2/core/v1/key/server",
+            };
 
-        // TODO: Other routes
+            pub static GET_SERVER_PUBLIC_IDCERT: Route = Route {
+                method: http::Method::GET,
+                path: "/.p2/core/v1/idcert/server",
+            };
+
+            pub static GET_SERVER_PUBLIC_KEY: Route = Route {
+                method: http::Method::GET,
+                path: "/.p2/core/v1/key/server",
+            };
+
+            pub static GET_ACTOR_IDCERTS: Route = Route {
+                method: http::Method::GET,
+                path: "/.p2/core/v1/idcert/actor/",
+            };
+
+            pub static UPDATE_SESSION_IDCERT: Route = Route {
+                method: http::Method::PUT,
+                path: "/.p2/core/v1/session/idcert/extern",
+            };
+
+            pub static DELETE_SESSION: Route = Route {
+                method: http::Method::DELETE,
+                path: "/.p2/core/v1/session/",
+            };
+
+            pub static ROTATE_SESSION_IDCERT: Route = Route {
+                method: http::Method::POST,
+                path: "/.p2/core/v1/session/idcert",
+            };
+
+            pub static UPLOAD_ENCRYPTED_PKM: Route = Route {
+                method: http::Method::POST,
+                path: "/.p2/core/v1/session/keymaterial",
+            };
+
+            pub static GET_ENCRYPTED_PKM: Route = Route {
+                method: http::Method::GET,
+                path: "/.p2/core/v1/session/keymaterial",
+            };
+
+            pub static DELETE_ENCRYPTED_PKM: Route = Route {
+                method: http::Method::DELETE,
+                path: "/.p2/core/v1/session/keymaterial",
+            };
+
+            pub static GET_ENCRYPTED_PKM_UPLOAD_SIZE_LIMIT: Route = Route {
+                method: http::Method::OPTIONS,
+                path: "/.p2/core/v1/session/keymaterial",
+            };
+        }
     }
 }
