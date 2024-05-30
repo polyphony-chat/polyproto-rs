fb2f7470460843b2477732a82e786e991d72bc8c
bitfl0wer <florian@pro-weber.com>
2024-05-30 16:08:33 +0200

    Add test get_server_id_cert

--- tests/api/core/mod.rs
+++ tests/api/core/mod.rs
@@ -3,11 +3,11 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use std::str::FromStr;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use der::asn1::{Uint, UtcTime};
-use httptest::matchers::request;
 use httptest::matchers::request::method_path;
+use httptest::matchers::{eq, json_decoded, request};
 use httptest::responders::json_encoded;
 use httptest::*;
 use polyproto::certs::capabilities::Capabilities;
@@ -15,7 +15,8 @@ use polyproto::certs::idcert::IdCert;
 use polyproto::certs::idcsr::IdCsr;
 use polyproto::key::PublicKey;
 use polyproto::types::routes::core::v1::{
-    GET_CHALLENGE_STRING, GET_SERVER_PUBLIC_KEY, ROTATE_SERVER_IDENTITY_KEY,
+    GET_CHALLENGE_STRING, GET_SERVER_PUBLIC_IDCERT, GET_SERVER_PUBLIC_KEY,
+    ROTATE_SERVER_IDENTITY_KEY,
 };
 use polyproto::Name;
 use serde_json::json;
@@ -125,4 +126,69 @@ async fn get_server_public_key() {
         public_key.public_key_bitstring,
         priv_key.public_key.public_key_info().public_key_bitstring
     );
+}
+
+#[tokio::test]
+async fn get_server_id_cert() {
+    init_logger();
+    let mut csprng = rand::rngs::OsRng;
+    let subject = Name::from_str("CN=root,DC=polyphony,DC=chat").unwrap();
+    let priv_key = Ed25519PrivateKey::gen_keypair(&mut csprng);
+    let id_csr = IdCsr::<Ed25519Signature, Ed25519PublicKey>::new(
+        &subject,
+        &priv_key,
+        &Capabilities::default_home_server(),
+        Some(polyproto::certs::Target::HomeServer),
+    )
+    .unwrap();
+    let id_cert = IdCert::from_ca_csr(
+        id_csr,
+        &priv_key,
+        Uint::new(&[8]).unwrap(),
+        subject,
+        Validity {
+            not_before: Time::UtcTime(
+                UtcTime::from_unix_duration(Duration::from_secs(10)).unwrap(),
+            ),
+            not_after: Time::UtcTime(
+                UtcTime::from_unix_duration(Duration::from_secs(1000)).unwrap(),
+            ),
+        },
+    )
+    .unwrap();
+    let cert_pem = id_cert.to_pem(der::pem::LineEnding::LF).unwrap();
+    let server = Server::run();
+    server.expect(
+        Expectation::matching(method_path(
+            GET_SERVER_PUBLIC_IDCERT.method.as_str(),
+            GET_SERVER_PUBLIC_IDCERT.path,
+        ))
+        .respond_with(json_encoded(json!(cert_pem))),
+    );
+
+    let url = server_url(&server);
+    let client = polyproto::api::HttpClient::new(&url).unwrap();
+    let cert = client
+        .get_server_id_cert::<Ed25519Signature, Ed25519PublicKey>(None)
+        .await
+        .unwrap();
+    assert_eq!(cert.to_pem(der::pem::LineEnding::LF).unwrap(), cert_pem);
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    server.expect(
+        Expectation::matching(all_of![
+            request::method(GET_SERVER_PUBLIC_IDCERT.method.as_str()),
+            request::path(GET_SERVER_PUBLIC_IDCERT.path),
+            request::body(json_decoded(eq(json!({"timestamp": timestamp})))),
+        ])
+        .respond_with(json_encoded(json!(cert_pem))),
+    );
+
+    let cert = client
+        .get_server_id_cert::<Ed25519Signature, Ed25519PublicKey>(Some(timestamp))
+        .await
+        .unwrap();
+    assert_eq!(cert.to_pem(der::pem::LineEnding::LF).unwrap(), cert_pem);
 }
