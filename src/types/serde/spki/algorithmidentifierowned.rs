d0bcef0d4509598016ffe6135ac47eb64af0a47e
bitfl0wer <florian@pro-weber.com>
2024-06-03 21:22:00 +0200

    impl serde Deserialize and Serialize on wrapper type for AlgorithmIdentifierOwned

--- src/types/serde/spki/algorithmidentifierowned.rs
+++ src/types/serde/spki/algorithmidentifierowned.rs
@@ -4,7 +4,9 @@
 
 use std::ops::{Deref, DerefMut};
 
-use der::Any;
+use der::{Any, Decode, Encode};
+use serde::de::Visitor;
+use serde::{Deserialize, Serialize};
 use spki::ObjectIdentifier;
 
 #[derive(Debug, PartialEq, Eq, Clone, PartialOrd, Ord)]
@@ -13,6 +15,16 @@ pub struct AlgorithmIdentifierOwned(spki::AlgorithmIdentifierOwned);
 impl AlgorithmIdentifierOwned {
     pub fn new(oid: ObjectIdentifier, parameters: Option<Any>) -> Self {
         Self(spki::AlgorithmIdentifierOwned { oid, parameters })
+    }
+
+    /// Try to encode this type as DER.
+    pub fn to_der(&self) -> Result<Vec<u8>, der::Error> {
+        self.0.to_der()
+    }
+
+    /// Try to decode this type from DER.
+    pub fn from_der(bytes: &[u8]) -> Result<AlgorithmIdentifierOwned, der::Error> {
+        spki::AlgorithmIdentifierOwned::from_der(bytes).map(Self)
     }
 }
 
@@ -27,5 +39,62 @@ impl Deref for AlgorithmIdentifierOwned {
 impl DerefMut for AlgorithmIdentifierOwned {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
+    }
+}
+
+impl From<spki::AlgorithmIdentifierOwned> for AlgorithmIdentifierOwned {
+    fn from(value: spki::AlgorithmIdentifierOwned) -> Self {
+        Self(value)
+    }
+}
+
+impl From<AlgorithmIdentifierOwned> for spki::AlgorithmIdentifierOwned {
+    fn from(value: AlgorithmIdentifierOwned) -> Self {
+        value.0
+    }
+}
+
+pub trait LikeAlgorithmIdentifierOwned {
+    fn new(oid: ObjectIdentifier, parameters: Option<Any>) -> Self;
+}
+
+impl LikeAlgorithmIdentifierOwned for AlgorithmIdentifierOwned {
+    fn new(oid: ObjectIdentifier, parameters: Option<Any>) -> Self {
+        Self::new(oid, parameters)
+    }
+}
+
+impl LikeAlgorithmIdentifierOwned for spki::AlgorithmIdentifierOwned {
+    fn new(oid: ObjectIdentifier, parameters: Option<Any>) -> Self {
+        spki::AlgorithmIdentifier { oid, parameters }
+    }
+}
+
+struct AlgorithmIdentifierVisitor;
+
+impl<'de> Visitor<'de> for AlgorithmIdentifierVisitor {
+    type Value = AlgorithmIdentifierOwned;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        formatter.write_str("this Visitor expects a DER encoded AlgorithmIdentifier with optional der::Any parameters and a BitString Key")
+    }
+}
+
+impl<'de> Deserialize<'de> for AlgorithmIdentifierOwned {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_bytes(AlgorithmIdentifierVisitor)
+    }
+}
+
+impl Serialize for AlgorithmIdentifierOwned {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let der = self.to_der().map_err(serde::ser::Error::custom)?;
+        serializer.serialize_bytes(&der)
     }
 }
