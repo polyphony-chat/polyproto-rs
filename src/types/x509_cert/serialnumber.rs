65d80d147bd13cd000cb8e9c9f706bc41c5e6679
bitfl0wer <florian@pro-weber.com>
2024-06-04 19:18:49 +0200

    Add documentation

--- src/types/x509_cert/serialnumber.rs
+++ src/types/x509_cert/serialnumber.rs
@@ -5,6 +5,31 @@
 use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+/// Wrapper type around [x509_cert::serial_number::SerialNumber], providing serde support, if the
+/// `serde` feature is enabled. See "De-/serialization value expectations" below for more
+/// information.
+///
+///   [RFC 5280 Section 4.1.2.2.]  Serial Number
+///
+///   The serial number MUST be a positive integer assigned by the CA to
+///   each certificate.  It MUST be unique for each certificate issued by a
+///   given CA (i.e., the issuer name and serial number identify a unique
+///   certificate).  CAs MUST force the serialNumber to be a non-negative
+///   integer.
+///
+///   Given the uniqueness requirements above, serial numbers can be
+///   expected to contain long integers.  Certificate users MUST be able to
+///   handle serialNumber values up to 20 octets.  Conforming CAs MUST NOT
+///   use serialNumber values longer than 20 octets.
+///
+///   Note: Non-conforming CAs may issue certificates with serial numbers
+///   that are negative or zero.  Certificate users SHOULD be prepared to
+///   gracefully handle such certificates.
+///
+/// ## De-/serialization value expectations
+///
+/// The serde de-/serialization implementation for [`SerialNumber`] expects a byte slice representing
+/// a positive integer.
 pub struct SerialNumber(::x509_cert::serial_number::SerialNumber);
 
 impl From<::x509_cert::serial_number::SerialNumber> for SerialNumber {
