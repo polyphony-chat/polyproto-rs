d1384aefb8a4e9f47e516ae0dcde997c10262dc3
bitfl0wer <florian@pro-weber.com>
2025-01-29 20:06:29 +0100

    add remaining tests

--- tests/types/gateway.rs
+++ tests/types/gateway.rs
@@ -1,5 +1,6 @@
 use polyproto::types::gateway::payload::{
-    Hello, Identify, ServiceChannel, ServiceChannelAck, ServiceChannelAction,
+    ActorCertificateInvalidation, Heartbeat, HeartbeatAck, Hello, Identify, NewSession, Resume,
+    Resumed, ServerCertificateChange, ServiceChannel, ServiceChannelAck, ServiceChannelAction,
 };
 use polyproto::types::gateway::{Event, Opcode, Payload};
 
@@ -94,4 +95,121 @@ fn serde_event_payload_service_ack() {
     let service_channel_ack_from_json =
         serde_json::from_str::<Event>(&service_channel_ack_json).unwrap();
     assert_eq!(service_channel_ack, service_channel_ack_from_json);
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_new_session() {
+    let new_session = Event {
+        n: "core".to_string(),
+        op: Opcode::NewSession as u16,
+        d: Payload::NewSession(NewSession {
+            cert: "cert".to_string(),
+        }),
+        s: Some(10),
+    };
+    let new_session_json = serde_json::to_string(&new_session).unwrap();
+    let new_session_from_json = serde_json::from_str::<Event>(&new_session_json).unwrap();
+    assert_eq!(new_session, new_session_from_json);
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_actor_certificate_invalidation() {
+    let actor_certificate_invalidation = Event {
+        n: "core".to_string(),
+        op: Opcode::ActorCertificateInvalidation as u16,
+        d: Payload::ActorCertificateInvalidation(ActorCertificateInvalidation {
+            serial: 234789,
+            invalid_since: 8923404,
+            signature: "signature".to_string(),
+        }),
+        s: Some(1),
+    };
+    let actor_certificate_invalidation_json =
+        serde_json::to_string(&actor_certificate_invalidation).unwrap();
+    let actor_certificate_invalidation_from_json =
+        serde_json::from_str::<Event>(&actor_certificate_invalidation_json).unwrap();
+    assert_eq!(
+        actor_certificate_invalidation,
+        actor_certificate_invalidation_from_json
+    );
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_resume() {
+    let resume = Event {
+        n: "core".to_string(),
+        op: Opcode::Resume as u16,
+        d: Payload::Resume(Resume { s: 12 }),
+        s: None,
+    };
+    let resume_json = serde_json::to_string(&resume).unwrap();
+    let resume_from_json = serde_json::from_str::<Event>(&resume_json).unwrap();
+    assert_eq!(resume, resume_from_json);
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_resumed() {
+    let resumed = Event {
+        n: "core".to_string(),
+        op: Opcode::Resumed as u16,
+        d: Payload::Resumed(Resumed { inner: Vec::new() }),
+        s: None,
+    };
+    let resumed_json = serde_json::to_string(&resumed).unwrap();
+    let resumed_from_json = serde_json::from_str::<Event>(&resumed_json).unwrap();
+    assert_eq!(resumed, resumed_from_json);
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_server_certificate_change() {
+    let certificate_change = Event {
+        n: "core".to_string(),
+        op: Opcode::ServerCertificateChange as u16,
+        d: Payload::ServerCertificateChange(ServerCertificateChange {
+            cert: "cert".to_string(),
+            old_invalid_since: 982314122,
+        }),
+        s: None,
+    };
+    let certificate_change_json = serde_json::to_string(&certificate_change).unwrap();
+    let certificate_change_from_json =
+        serde_json::from_str::<Event>(&certificate_change_json).unwrap();
+    assert_eq!(certificate_change, certificate_change_from_json);
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_server_heartbeat() {
+    let event = Event {
+        n: "core".to_string(),
+        op: Opcode::Heartbeat as u16,
+        d: Payload::Heartbeat(Heartbeat {
+            from: 0,
+            to: 100,
+            except: [2, 4, 6, 7, 8].to_vec(),
+        }),
+        s: None,
+    };
+    let event_json = serde_json::to_string(&event).unwrap();
+    let event_from_json = serde_json::from_str::<Event>(&event_json).unwrap();
+    assert_eq!(event, event_from_json);
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn serde_event_payload_server_heartbeat_ack() {
+    let event = Event {
+        n: "core".to_string(),
+        op: Opcode::HeartbeatAck as u16,
+        d: Payload::HeartbeatAck(HeartbeatAck { inner: [].to_vec() }),
+        s: None,
+    };
+    let event_json = serde_json::to_string(&event).unwrap();
+    let event_from_json = serde_json::from_str::<Event>(&event_json).unwrap();
+    assert_eq!(event, event_from_json);
 }
