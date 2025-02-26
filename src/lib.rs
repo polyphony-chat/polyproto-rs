46820bc5923a89bc4a94890fb0463a198f0f7a37
bitfl0wer <florian@pro-weber.com>
2025-02-26 14:46:33 +0100

    Add missing license headers, consolidate slightly differing license headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 /*!
 <img src="https://cloud.bitfl0wer.de/apps/files_sharing/publicpreview/2qCxoXJ27yW7QNR?file=/&fileId=1143147&x=256&y=256&a=true" align="left" alt="a purple cog, split in the middle along the horizontal axis with a gap inbetween the two halves. three overlayed, offset sinus-like waves travel through that gap. each wave has a different shade of purple" width="128px" height="auto"></img>
--- tests/types/cacheable_cert.rs
+++ tests/types/cacheable_cert.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 use polyproto::api::core::cacheable_cert::CacheableIdCert;
 use polyproto::key::{PrivateKey, PublicKey};
 use polyproto::signature::Signature;
