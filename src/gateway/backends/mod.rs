74514cdf623109349a4292d1e92ae4d58fe37260
bitfl0wer <florian@pro-weber.com>
2025-03-02 21:26:58 +0100

    i dont wanna talk about it

--- src/gateway/backends/mod.rs
+++ src/gateway/backends/mod.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+pub(crate) mod heartbeat;
+
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::sync::Arc;
