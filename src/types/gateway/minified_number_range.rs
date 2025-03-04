193b08337ae6dcd9ff8fecdbe75a1d4479506c63
bitfl0wer <florian@pro-weber.com>
2025-03-04 22:39:39 +0100

    add todo impls (unfinished!) From<&Vec<u64>> for MinifiedNumberRange From<MinifiedNumberRange> for Heartbeat

--- src/types/gateway/minified_number_range.rs
+++ src/types/gateway/minified_number_range.rs
@@ -1,6 +1,20 @@
+use super::payload::Heartbeat;
+
 #[derive(Clone, Debug, Default, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct MinifiedNumberRange {
     pub from: u64,
     pub to: u64,
     pub except: Vec<u64>,
+}
+
+impl From<&Vec<u64>> for MinifiedNumberRange {
+    fn from(value: &Vec<u64>) -> Self {
+        todo!()
+    }
+}
+
+impl From<MinifiedNumberRange> for Heartbeat {
+    fn from(value: MinifiedNumberRange) -> Self {
+        todo!()
+    }
 }
