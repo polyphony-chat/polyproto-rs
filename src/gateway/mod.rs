47b3912801a91fb05d01998c0edc533d97143b16
bitfl0wer <florian@pro-weber.com>
2025-03-05 13:25:16 +0100

    remove sequence channel, add some documentation

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -19,6 +19,13 @@ use tokio::task::JoinHandle;
 pub(crate) static KILL_LOG_MESSAGE: &str = "Received kill signal, shutting down";
 
 #[derive(Debug)]
+/// A generic gateway, making no assumptions about the gateway backend in use. Communication is
+/// abstracted through the use of [tokio::sync::watch] channels, making sending and consumption of
+/// messages to/from a gateway server trivial.
+///
+/// [Gateway] handles most of the gateway connection lifecycle. This includes building a connection,
+/// parsing [Hello] information and exhibiting proper [Heartbeat] behavior, including responding to
+/// [RequestHeartbeat] messages.
 pub struct Gateway<S: Signature, T: PrivateKey<S>>
 where
     S: Debug,
@@ -26,10 +33,15 @@ where
 {
     /// A reference to a corresponding [Session].
     pub session: Arc<Session<S, T>>,
+    /// This channel can be used to send [GatewayMessages](GatewayMessage) to the gateway server.
     pub send_channel: watch::Sender<GatewayMessage>,
+    /// This channel can be used to receive [GatewayMessages](GatewayMessage) from the gateway server.
     pub receive_channel: watch::Receiver<GatewayMessage>,
     kill_send: watch::Sender<Closed>,
+    /// Tokio task running "receiver" logic
     receiver_task: JoinHandle<()>,
+    /// Tokio task running "sender" logic
     sender_task: JoinHandle<()>,
+    /// Tokio task running heartbeat logic
     heartbeat_task: Heartbeat,
 }
