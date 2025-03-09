ab0449c836eb0e4d6457ac1cd19ba87f71617169
bitfl0wer <florian@pro-weber.com>
2025-03-09 12:33:42 +0100

    license headers

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -3,5 +3,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 # Execute this from the project root:
-# $: ./scripts/check-license-header.sh
-npx github:viperproject/check-license-header#v2 check -c .github/license-check/config.json --path .
+# $: ./scripts/fmt.sh
+cargo fmt && cargo clippy --fix --allow-dirty --allow-staged
