8719db42baa76751802b1e320e01f614c9d4f5f3
bitfl0wer <florian@pro-weber.com>
2025-03-20 23:22:45 +0100

    start work on accesscontrol type

--- src/types/rawr.rs
+++ src/types/rawr.rs
@@ -2,5 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use super::Identifer;
+
 pub struct Resource;
 pub struct ResourceInformation;
+
+// TODO: Derives, including Serde derives
+pub struct AccessControl {
+    pub private: bool,
+    pub allowlist: Vec<Identifer>,
+    pub denylist: Vec<Identifer>,
+}
