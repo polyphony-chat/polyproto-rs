35d5f77404bfabceea36f6157a602486f52f2cc1
bitfl0wer <florian@pro-weber.com>
2025-03-21 17:53:18 +0100

    impl Resource with reqwest `Form` conversion support

--- src/types/rawr.rs
+++ src/types/rawr.rs
@@ -4,8 +4,25 @@
 
 use super::Identifer;
 
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+/// Standard HTTP file.
 pub struct Resource {
-    pub content_type: String,
+    /// MIME content type of this resource
+    pub content_type: Option<String>,
+    /// File name of this resource
+    pub filename: Option<String>,
+    /// Contents, as raw bytes
+    pub contents: Vec<u8>,
+}
+
+#[cfg(feature = "reqwest")]
+impl Resource {
+    /// Tries to convert [Self] into a [::reqwest::multipart::Form]. Conversion will fail,
+    /// if `content_type` is present and cannot be parsed as [mime::Mime] by `reqwest`
+    pub fn into_multipart(self) -> Result<::reqwest::multipart::Form, crate::errors::InvalidInput> {
+        use ::reqwest::multipart::Form;
+        Form::try_from(self)
+    }
 }
 
 #[cfg_attr(feature = "serde", serde_with::serde_as)]
@@ -33,4 +50,38 @@ pub struct ResourceAccessProperties {
     pub allowlist: Vec<Identifer>,
     /// A list of actors and/or instances who cannot have access to this resource.
     pub denylist: Vec<Identifer>,
+}
+
+#[cfg(feature = "reqwest")]
+mod reqwest {
+    use reqwest::multipart::{Form, Part};
+
+    use crate::errors::InvalidInput;
+
+    use super::Resource;
+
+    impl TryFrom<Resource> for Form {
+        type Error = InvalidInput;
+        /// Performs the conversion. Conversion will fail, if `content_type` is present and cannot
+        /// be parsed as [mime::Mime] by `reqwest`
+        fn try_from(resource: Resource) -> Result<Self, Self::Error> {
+            let mut form = Form::new();
+
+            let mut part = Part::bytes(resource.contents);
+
+            if let Some(content_type) = resource.content_type {
+                part = part
+                    .mime_str(&content_type)
+                    .map_err(|e| InvalidInput::Malformed(e.to_string()))?;
+            }
+
+            if let Some(filename) = resource.filename {
+                part = part.file_name(filename);
+            }
+
+            form = form.part("file", part);
+
+            Ok(form)
+        }
+    }
 }
