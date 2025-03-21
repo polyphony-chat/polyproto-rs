a3f2b9d356fac82e8f26e1073af6a90b46a85348
bitfl0wer <florian@pro-weber.com>
2025-03-21 13:33:10 +0100

    test deserialize, serialize impl for domain name

--- tests/types/federation_id.rs
+++ tests/types/federation_id.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use polyproto::types::FederationId;
-use serde_json::{Value, to_value};
+use polyproto::types::{DomainName, FederationId};
+use serde_json::{Value, from_value, to_value};
 
 #[cfg(feature = "types")]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
@@ -35,7 +35,7 @@ fn test_correct_federation_ids() {
 #[cfg(feature = "types")]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
 #[cfg_attr(not(target_arch = "wasm32"), test)]
-fn test_serde_serialize() {
+fn test_fid_serde_serialize() {
     const NAME: &str = "xenia@example.com";
     let id = FederationId::new(NAME).unwrap();
     let json = to_value(id).unwrap();
@@ -47,11 +47,34 @@ fn test_serde_serialize() {
 #[cfg(feature = "types")]
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
 #[cfg_attr(not(target_arch = "wasm32"), test)]
-fn test_serde_deserialize() {
+fn test_fid_serde_deserialize() {
     use serde_json::from_value;
 
     const NAME: &str = "xenia@example.com";
     let json_value = Value::String(NAME.to_string());
     let id: FederationId = from_value(json_value).unwrap();
     assert_eq!(&id.to_string(), NAME);
+}
+
+#[cfg(feature = "serde")]
+#[cfg(feature = "types")]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn test_domain_name_serde_serialize() {
+    const NAME: &str = "xenia@example.com";
+    let domain_name = DomainName::new(NAME).unwrap();
+    let json = to_value(domain_name).unwrap();
+    assert!(json.is_string());
+    assert_eq!(json, Value::String(NAME.to_string()));
+}
+
+#[cfg(feature = "serde")]
+#[cfg(feature = "types")]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test::wasm_bindgen_test)]
+#[cfg_attr(not(target_arch = "wasm32"), test)]
+fn test_domain_name_serde_deserialize() {
+    const NAME: &str = "xenia@example.com";
+    let json_value = Value::String(NAME.to_string());
+    let domain_name: DomainName = from_value(json_value).unwrap();
+    assert_eq!(domain_name.to_string().as_str(), NAME);
 }
