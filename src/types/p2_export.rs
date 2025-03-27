29fbe9bc103ab90426a762507a2a1d47c65e39d8
bitfl0wer <florian@pro-weber.com>
2025-03-27 13:23:01 +0100

    i think this fits better. idk

--- src/types/p2_export.rs
+++ src/types/p2_export.rs
@@ -3,22 +3,28 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 use std::collections::HashMap;
+use std::ffi::OsString;
 
 use super::ResourceAccessProperties;
 
 /// A P2Export data export.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct P2Export<M> {
     /// Generic messages
     messages: MessageBatches<M>,
+    /// File name
+    name: OsString,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MessageBatches<M> {
     rawr: Option<RawrContent<M>>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RawrContent<M> {
-    resources: Vec<M>,
-    access_properties: HashMap<String, ResourceAccessProperties>,
+    resources: HashMap<OsString, M>,
+    access_properties: HashMap<OsString, ResourceAccessProperties>,
 }
 
 #[cfg(feature = "reqwest")]
