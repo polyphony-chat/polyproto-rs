b037885f7cdbe382585b50b4a812f71783cd3711
bitfl0wer <florian@pro-weber.com>
2025-03-27 11:23:17 +0100

    add license headers

--- src/wasm_bindgen/utils.rs
+++ src/wasm_bindgen/utils.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
